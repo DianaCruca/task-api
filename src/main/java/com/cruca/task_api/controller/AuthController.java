2831e98a0c222a1f09468f6dd5f6d10daa98775b
Diana Cruz <dian.cruca@gmail.com>
2025-04-24 23:06:08 -0600

    docs(user): add swagger documentation

--- src/main/java/com/cruca/task_api/controller/AuthController.java
+++ src/main/java/com/cruca/task_api/controller/AuthController.java
@@ -7,6 +7,11 @@ import com.cruca.task_api.dto.UserDtoResponse;
 import com.cruca.task_api.exception.BadRequestException;
 import com.cruca.task_api.payload.CorrectRequest;
 import com.cruca.task_api.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -21,6 +26,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
+@Tag(
+        name = "Auth Controller",
+        description = "This controller provides functionalities to create users and log-in"
+)
 @RestController
 @RequestMapping("/api/public/user")
 public class AuthController {
@@ -28,6 +37,29 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    @Operation(
+            summary = "Register a new user by any user, without privileges",
+            description = " Register a new user by any user, without assign privileges | Authentication: Not required",
+            tags = { "Controller" },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Register user with name, lastname, email, password",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = UserDtoRequest.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Usuario creado correctamente",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDtoResponse.class)
+                            )
+                    )
+            }
+    )
     @PreAuthorize("permitAll()")
     @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createUserPublic(@Valid @RequestBody UserDtoRequest userDtoRequest) {
@@ -46,6 +78,29 @@ public class AuthController {
         }
     }
 
+    @Operation(
+            summary = "Login",
+            description ="Login for Registered Users | Authentication: Not required",
+            tags = { "Controller" },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Login with email and password",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = LoginDtoRequest.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Usuario creado correctamente",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = LoginDtoResponse.class)
+                            )
+                    )
+            }
+    )
     @PreAuthorize("permitAll()")
     @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> login(@RequestBody LoginDtoRequest dtoRequest) {
