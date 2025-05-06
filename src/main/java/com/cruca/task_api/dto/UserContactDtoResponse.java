8a9a16d5979e43eeb75b0dc6467da04b63872e55
Diana Cruz <dian.cruca@gmail.com>
2025-05-05 18:05:05 -0600

    feat(user): implement contact data transformation for user

---
+++ src/main/java/com/cruca/task_api/dto/UserContactDtoResponse.java
@@ -0,0 +1,12 @@
+package com.cruca.task_api.dto;
+
+import lombok.Data;
+
+@Data
+public class UserContactDtoResponse {
+
+    private Long idUser;
+    private String nameUser;
+    private String lastnameUser;
+
+}
