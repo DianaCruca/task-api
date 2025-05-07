814b462e2567a2799b5c4813d5e3dbe8f225e2fc
Diana Cruz <dian.cruca@gmail.com>
2025-05-07 00:28:16 -0600

    fix(user): add bidirectional relationship with project task

--- src/main/java/com/cruca/task_api/model/User.java
+++ src/main/java/com/cruca/task_api/model/User.java
@@ -57,6 +57,9 @@ public class User implements UserDetails {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "createdBy", orphanRemoval = true)
     private List<Project> createdProjects;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "createdBy", orphanRemoval = true)
+    private List<ProjectTask> createdProjectTasks;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user", orphanRemoval = true)
     private List<ProjectMember> projectsMember;
 
